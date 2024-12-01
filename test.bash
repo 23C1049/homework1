fe07ec6250f878c75412677004f8f780dc382e5b
Shizen Kotake <s23c1049pa@s.chibakoudai.jp>
2024-12-01 14:21:12 +0900

    test.bashの追加

---
+++ test.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Shizen Kotake
+# SPDX-License-Identifier: BSD-3-Clause
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+### NORMAL INPUT ###
+out=$(echo 長野県 |./preflower)
+[ "${out}" = "長野県の県花はリンドウです　花言葉は高貴" ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+echo 名古屋県| ./preflower 2> /tmp/test_file.log
+[ "$?" = 1 ] || ng "$LINENO"
+grep 存在しない都道府県 /tmp/test_file.log
+[ "$?" = 0 ] || ng "$LINENO"
+
+
+echo | ./preflower 2> /tmp/test_file.log
+[ "$?" = 1 ] || ng "$LINENO"
+grep 存在しない都道府県 /tmp/test_file.log
+[ "$?" = 0 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit $res
